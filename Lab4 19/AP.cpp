3816c4c618b46afdd16dee8bfff6cf5ddbf7e4da
Alexandr <humeruzz0@gmail.com>
2019-10-06 18:46:41 +0300

    release methods

--- Lab4 19/AP.cpp
+++ Lab4 19/AP.cpp
@@ -1,1 +1,33 @@
 #include "AP.h"
+
+AP::AP()
+{
+}
+
+AP::AP(string, string, string, int)
+{
+}
+
+void AP::APinput()
+{
+}
+
+void AP::APsort(int)
+{
+}
+
+void AP::APprint()
+{
+}
+
+auto AP::APsearch()
+{
+}
+
+void AP::APBoutput()
+{
+}
+
+void AP::APBinput()
+{
+}
